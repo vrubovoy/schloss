3d5f97f245197514bba2a82123ba05f40f3b28b2
zudaR107 <zudin_daniil@mail.ru>
2026-08-20 09:12:04 +0300

    feat: bump schloss-ui and add the shared Not Found page
    Closes #154

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,13 @@
 import { createRouter, createRootRoute, createRoute, Outlet } from '@tanstack/react-router'
+import { NotFoundPage } from '@zudar107/schloss-ui'
 import HomePage from '../pages/HomePage'
 import HelpPage from '../pages/HelpPage'
 import { AuthCallbackPage } from '../features/auth/AuthCallbackPage'
+import { HeroIllustration } from '../components/HeroIllustration'
 
 const rootRoute = createRootRoute({
   component: () => <Outlet />,
+  notFoundComponent: () => <NotFoundPage homeHref="/" illustration={<HeroIllustration size={100} />} />,
 })
 
 const indexRoute = createRoute({
